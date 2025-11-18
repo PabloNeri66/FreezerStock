55b27972c36a99b22525c147a8148670dc82a80c
Pablo Alves Neri <pabloneri66@gmail.com>
2025-11-18 16:04:55 -0300

    Feat/Grafico-qtd-vendas-diarias + grafico-Sabor

--- core/views.py
+++ core/views.py
@@ -7,11 +7,16 @@ def home(request):
     geladinho_metrics = metrics.get_geladinho_metrics()
     sales_metrics = metrics.get_sales_metrics()
     daily_sales_data = metrics.get_daily_sales_data()
+    daily_sales_quantity_data = metrics.get_daily_sales_quantity_data()
+    graphic_geladinho_flavor_metric, geladinho_colors = metrics.get_graphic_geladinho_flavor_metric()
 
     context = {
         'geladinho_metrics': geladinho_metrics,
         'sales_metrics': sales_metrics,
         'daily_sales_data': json.dumps(daily_sales_data),
+        'daily_sales_quantity_data': json.dumps(daily_sales_quantity_data),
+        'geladinho_count_by_flavor': json.dumps(graphic_geladinho_flavor_metric),
+        'geladinho_colors': geladinho_colors,
     }
 
     return render(request, 'home.html', context)
