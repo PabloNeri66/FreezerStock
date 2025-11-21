3d58c28ec96dc272f04c34f71667f02684e42c07
Pablo Alves Neri <pabloneri66@gmail.com>
2025-11-21 15:02:57 -0300

    global Permission Class e Auth Global settings

---
+++ authentication/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from rest_framework_simplejwt import views
+
+urlpatterns = [
+    path(
+        'authentication/token/',
+        views.TokenObtainPairView.as_view(),
+        name='token_obtain_pair',
+    ),
+    path(
+        'authentication/token/refresh/',
+        views.TokenRefreshView.as_view(),
+        name='token_refresh',
+    ),
+    path(
+        'authentication/token/verify/',
+        views.TokenVerifyView.as_view(),
+        name='token_verify',
+    ),
+]
--- core/urls.py
+++ core/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 
+    path('api/v1/', include('authentication.urls')),
     path('', include('geladinhos.urls')),
     path('', include('inflows.urls')),
     path('', include('outflows.urls')),
