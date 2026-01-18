f4e98482eec0a99d2ae7f4a8034d2b7b68fbf301
Pablo Alves Neri <pabloneri66@gmail.com>
2026-01-17 23:24:47 -0300

    t push origin main Revert "Merge pull request #24 from PabloNeri66/feat/export_ouftflows"
    This reverts commit 096f5af47a4a0816b2a9956705c13b6cd227fd12, reversing
    changes made to 2b4a4626cc97e6b82b5992034fba67c259c72623.

--- outflows/views.py
+++ outflows/views.py
@@ -3,10 +3,8 @@ from django.contrib.auth.mixins import (
     LoginRequiredMixin,
     PermissionRequiredMixin,
 )
-from django.http import JsonResponse
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
-from django.views import View
 from django.views.decorators.cache import cache_page
 from django.views.generic import (
     ListView,
@@ -27,7 +25,6 @@ from core import metrics, permissions
 from .forms import OutflowForm
 from .models import Outflow
 from .serializers import OutflowSerializer
-from .tasks import csv_outflows_generator
 
 
 class OutflowListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
@@ -70,7 +67,7 @@ class OutflowDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView)
 class OutflowListCreateApiView(ListCreateAPIView):
     queryset = Outflow.objects.all()
     serializer_class = OutflowSerializer
-    permission_classes = [permissions.GlobalDefaultPermission]  # Qg fixed kkk
+    # permission_classes = [permissions.GlobalDefaultPermission]
 
     @method_decorator(cache_page(60 * 15, key_prefix='outflow_list'))
     def list(self, request, *args, **kwargs):
@@ -81,13 +78,3 @@ class OutflowRetrieveApiView(RetrieveAPIView):
     queryset = Outflow.objects.all()
     serializer_class = OutflowSerializer
     permission_classes = [permissions.GlobalDefaultPermission]
-
-
-# Tasks
-class OutflowCsvGenerator(View):
-    def post(self, request):
-        task = csv_outflows_generator.delay()
-        return JsonResponse({
-            'status': 'processando',
-            'task_id': task.id
-        })
